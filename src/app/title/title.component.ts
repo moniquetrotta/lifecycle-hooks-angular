69f79717620701c87ce1ff2e3f282ac22c86ef68
moniquetrotta <monique.trotta@gmail.com>
2023-09-25 11:48:30 -0300

    lifecycle hooks: oninit, onchanges, input

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit, OnChanges {
+   @Input() nome:string = ''
+
+  constructor() { 
+    console.log(`Construtor ${this.nome}`);
+  }
+  ngOnChanges(): void {
+    console.log(`OnChanges ${this.nome}`)
+  }
+  ngOnInit(): void {
+    // this.nome = `ola ${this.nome}`
+    this.nome = this.nome + "x"
+    console.log(`OnInit ${this.nome}`);
+  }
+
+
+}
