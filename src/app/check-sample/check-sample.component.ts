246fc1ec51aa8cba8352941a0deed98c7b8bd2ae
moniquetrotta <monique.trotta@gmail.com>
2023-09-25 16:56:57 -0300

    Incrementar e Decrementar com Docheck

--- src/app/check-sample/check-sample.component.ts
+++ src/app/check-sample/check-sample.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit, DoCheck, AfterContentChecked, AfterContentInit, Afte
 })
 export class CheckSampleComponent implements OnInit, DoCheck, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit {
 
+  quantidade:number = 0
+
   constructor() { }
+
+  adicionar (){
+    this.quantidade += 1
+  }
+  decrementar (){
+    this.quantidade -= 1
+  }
+
   ngDoCheck(): void {
       console.log("ngDoCheck")
   }
